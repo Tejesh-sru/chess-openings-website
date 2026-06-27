e507005db6f0be670acd7910fe33e1101a77fadd
Tejesh Jakkula <jakkulatejesh2005@gmail.com>
2026-06-27 11:52:46 +0530

    Remove unnecessary blank line in index.js

--- mern-backend/src/index.js
+++ mern-backend/src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-
+ 
 dotenv.config();
 
 const app = express();
