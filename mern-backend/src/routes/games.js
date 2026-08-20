d1f21df1a0664338958a9952f4a46b9649452c9e
Tejesh Jakkula <jakkulatejesh2005@gmail.com>
2026-08-20 21:27:31 +0530

    Fix formatting in games.js

--- mern-backend/src/routes/games.js
+++ mern-backend/src/routes/games.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express') 
 const router = express.Router()
 const Game = require('../models/Game')
 const auth = require('../middleware/auth')
